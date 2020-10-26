3d1a0ab210f0f7714e56ae391a189a242ae44a5b
micnem <michael.nemni@gmail.com>
2020-10-26 12:39:33 +0200

    updates

---
+++ Week2/Day2/examples/script.js
@@ -0,0 +1,24 @@
+let person = {
+    username: "jonno",
+    password: "doe"
+  };
+
+let database = [person];
+let newsfeed = [];
+    
+let sam = {
+        username: "samo",
+        status: "I am a samo"
+    }
+let alon = {
+        username: "alone",
+        status: "I love it"
+    }
+let jake={
+        username: "cat",
+        status: "Jello"
+    }
+    
+  newsfeed.push(sam, alon, jake);
+  console.log('Newsfeed:', newsfeed);
+  
