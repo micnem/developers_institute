d501e30567ff02de2e46842790fdbdc79ac50f71
micnem <michael.nemni@gmail.com>
2020-11-08 16:44:56 +0200

    fix

---
+++ Week4/Day1/DailyChallenge/dailyChallenge.py
@@ -0,0 +1,17 @@
+str = input("Input a string (10 characters): ")
+empty = ""
+if len(str)<10:
+    print("not long enough")
+elif len(str)>10:
+    print("toooo long")
+else:
+    # print(str[0:5])
+    for i in range(len(str)):
+        empty += str[i:i+1]
+        print(empty)
+
+import random
+shuffled = ''.join(random.sample(str, len(str)))
+print(shuffled)
+# import string_utils
+# print(string_utils.shuffle(str))
