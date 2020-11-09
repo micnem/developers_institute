4055938ec41c8726876d326cfd5d8efa44456be6
micnem <michael.nemni@gmail.com>
2020-11-09 09:52:51 +0200

    corrections

--- Week4/Day1/DailyChallenge/dailyChallenge.py
+++ Week4/Day1/DailyChallenge/dailyChallenge.py
@@ -1,17 +1,20 @@
-str = input("Input a string (10 characters): ")
+import random
+
+text = input("Input a string (10 characters): ")
 empty = ""
-if len(str)<10:
+if len(text)<10:
     print("not long enough")
-elif len(str)>10:
+elif len(text)>10:
     print("toooo long")
 else:
     # print(str[0:5])
-    for i in range(len(str)):
-        empty += str[i:i+1]
+    for i in range(len(text)):
+        empty += text[i:i+1]
         print(empty)
-
-import random
-shuffled = ''.join(random.sample(str, len(str)))
-print(shuffled)
+        # first and last letters
+        # print([text[1], text[-1]]) 
+        
+shuffled = ''.join(random.sample(text, len(text)))
+print(f"this is your shuffled string: {shuffled}")
 # import string_utils
 # print(string_utils.shuffle(str))
