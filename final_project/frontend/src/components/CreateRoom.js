c474d32e6f98c278105851b63ce96afe8d087383
micnem <michael.nemni@gmail.com>
2020-12-28 13:05:23 +0200

    final project start

---
+++ final_project/frontend/src/components/CreateRoom.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export default class CreateRoom extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return <p>This is where you create a room...</p>
+    }
+}
